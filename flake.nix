8eb7c5431c8dee842c4e05bfc4ad5b4616698edc
Ivan Subotic <400790+subotic@users.noreply.github.com>
2024-04-26 10:14:25 +0200

    build: use nix for patched libtiff (ongoing)

--- flake.nix
+++ flake.nix
@@ -7,64 +7,68 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-  flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"] (system:
-    let
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
+      let
         overlays = [
-            (final: prev: {
-                abseil-cpp = prev.callPackage ./nix-overlays/abseil-cpp {
-                    cxxStandard = "23";
-                    # stdenv = prev.llvmPackages_17.stdenv;
-                };
-                protobuf = prev.callPackage ./nix-overlays/protobuf {
-                    cxxStandard = "23";
-                    # stdenv = prev.llvmPackages_17.stdenv;
-                };
-                # The iiif-validator and opentelemetry-cpp are not yet part of nixpkgs, so we need to add it as an overlay
-                iiif-validator = prev.callPackage ./nix-overlays/iiif-validator { };
-                opentelemetry-cpp = final.callPackage ./nix-overlays/opentelemetry-cpp {
-                    cxxStandard = "23";
-                    # stdenv = prev.llvmPackages_17.stdenv;
-                };
-                libtiff-patched = prev.callPackage ./nix-overlays/libtiff {
-                    cxxStandard = "23";
-                    # stdenv = prev.llvmPackages_17.stdenv;
-                    jbigkit = prev.pkgsStatic.jbigkit;
-                    # lerc = prev.pkgsStatic.lerc;
-                    libdeflate = prev.pkgsStatic.libdeflate;
-                    libjpeg_original = prev.pkgsStatic.libjpeg_original;
-                    libpng = prev.pkgsStatic.libpng;
-                    # libwebp = prev.pkgsStatic.libwebp;
-                    # libyuv = prev.pkgsStatic.libyuv;
-                    xz = prev.pkgsStatic.xz;
-                    zlib = prev.pkgsStatic.zlib;
-                    zstd = prev.pkgsStatic.zstd;
-                };
-            })
+          (final: prev: {
+            abseil-cpp = prev.callPackage ./nix-overlays/abseil-cpp {
+              cxxStandard = "23";
+              # stdenv = prev.llvmPackages_17.stdenv;
+            };
+            protobuf = prev.callPackage ./nix-overlays/protobuf {
+              cxxStandard = "23";
+              # stdenv = prev.llvmPackages_17.stdenv;
+            };
+            # The iiif-validator and opentelemetry-cpp are not yet part of nixpkgs, so we need to add it as an overlay
+            iiif-validator = prev.callPackage ./nix-overlays/iiif-validator { };
+            opentelemetry-cpp = final.callPackage ./nix-overlays/opentelemetry-cpp {
+              cxxStandard = "23";
+              # stdenv = prev.llvmPackages_17.stdenv;
+            };
+            libtiff-patched = prev.callPackage ./nix-overlays/libtiff {
+              cxxStandard = "23";
+              stdenv = prev.gcc13Stdenv;
+              jbigkit = prev.pkgsStatic.jbigkit;
+              # lerc = prev.pkgsStatic.lerc;
+              libdeflate = prev.pkgsStatic.libdeflate;
+              libjpeg_original = prev.pkgsStatic.libjpeg_original;
+              libpng = prev.pkgsStatic.libpng;
+              # libwebp = prev.pkgsStatic.libwebp;
+              # libyuv = prev.pkgsStatic.libyuv;
+              xz = prev.pkgsStatic.xz;
+              zlib = prev.pkgsStatic.zlib;
+              zstd = prev.pkgsStatic.zstd;
+            };
+          })
         ];
         pkgs = import nixpkgs {
-            inherit system overlays;
-            config = {
-                 packageOverrides = pkgs: rec {};
-            };
+          inherit system overlays;
+          config = {
+            packageOverrides = pkgs: rec { };
+          };
         };
-    in
-    {
+      in
+      {
         devShells = {
           # default = pkgs.mkShell.override {stdenv = pkgs.llvmPackages_17.stdenv;} {
-          default = pkgs.mkShell {
+          default = pkgs.mkShell.override { stdenv = pkgs.gcc13Stdenv; } {
             name = "sipi";
 
             nativeBuildInputs = [ pkgs.git pkgs.cmake pkgs.openssl pkgs.cacert pkgs.autoconf ];
 
             shellHook = ''
-                # echo "C++ includes: $(clang++ -E -x c++ - -v < /dev/null 2>&1 | grep '/include')"
-                # echo | clang -v -E -x c++ -
-                # export CXXFLAGS="-isystem ${pkgs.libcxx}/include/c++/v1 $CXXFLAGS"
-                # export LDFLAGS="-L${pkgs.libcxx}/lib $LDFLAGS"
-                export PS1="\\u@\\h | nix-develop> "
+              # echo "C++ includes: $(clang++ -E -x c++ - -v < /dev/null 2>&1 | grep '/include')"
+              # export CXXFLAGS="-isystem ${pkgs.libcxx}/include/c++/v1 $CXXFLAGS"
+              # export LDFLAGS="-L${pkgs.libcxx}/lib $LDFLAGS"
+              # echo | clang++ -v -E -x c++ -
+              # export NIX_CFLAGS_COMPILE="-isystem ${pkgs.glibc.dev}/include $NIX_CFLAGS_COMPILE"
+              # export NIX_CFLAGS_COMPILE="-isystem ${pkgs.gcc.cc.lib}/include $NIX_CFLAGS_COMPILE"
+              echo | g++ -Wp,-v -x c++ - -fsyntax-only
+              export PS1="\\u@\\h | nix-develop> "
             '';
 
             buildInputs = with pkgs; [
+              stdenv
               # List other packages you want in your devShell
               # C++ Compiler is already part of stdenv
               # Build tool
@@ -121,32 +125,32 @@
 
               iiif-validator # our own overlay
             ] ++ (with pkgs.pkgsStatic; [
-                # static variants of the libraries
-                bzip2
-                curl
-                expat
-                # jbigkit
-                # libjpeg_original
-                # libpng
-                libwebp
-                openssl
-                # xz
-                # zlib
-                # zstd
-            ]) ++ [stdenv.cc.libc];
+              # static variants of the libraries
+              bzip2
+              curl
+              expat
+              # jbigkit
+              # libjpeg_original
+              # libpng
+              libwebp
+              openssl
+              # xz
+              # zlib
+              # zstd
+            ]) ++ [ stdenv.cc.libc ];
           };
         };
 
         # The `callPackage` automatically fills the parameters of the function
         # in package.nix with what's inside the `pkgs` attribute.
-        packages.default = pkgs.callPackage ./package.nix  {
-            inherit (pkgs) abseil-cpp libtiff-static protobuf opentelemetry-cpp zlib;
-            cxxStandard = "23";
-            # stdenv = pkgs.clang17Stdenv;
+        packages.default = pkgs.callPackage ./package.nix {
+          inherit (pkgs) abseil-cpp libtiff-static protobuf opentelemetry-cpp zlib;
+          cxxStandard = "23";
+          stdenv = pkgs.gcc13Stdenv;
         };
 
         # The `config` variable contains our own outputs, so we can reference
         # neighbor attributes like the package we just defined one line earlier.
         # devShells.default = config.packages.default;
-    });
+      });
 }
