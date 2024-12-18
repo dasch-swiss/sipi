d1693c6150f3435db625710f05dfee39827a2d72
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2024-12-18 11:44:38 +0100

    add tests

--- test/unit/sipiimage/sipiimage.cpp
+++ test/unit/sipiimage/sipiimage.cpp
@@ -6,6 +6,7 @@
 #include "gtest/gtest.h"
 
 #include "../../../src/SipiImage.hpp"
+#include "../../../src/SipiImageError.hpp"
 #include "SipiIOTiff.h"
 
 // small function to check if file exist
@@ -39,6 +40,7 @@ const std::string wrongrotation = "../../../../test/_test_data/images/unit/image
 const std::string watermark_correct = "../../../../test/_test_data/images/unit/watermark_correct.tif";
 const std::string watermark_incorrect = "../../../../test/_test_data/images/unit/watermark_incorrect.tif";
 const std::string tiffJpegScanlineBug = "../../../../test/_test_data/images/knora/tiffJpegScanlineBug.tif";
+const std::string jpegCorrupt = "../../../../test/_test_data/images/unit/jpegCorrupt.jpg";
 
 // Check if configuration file can be found
 TEST(SipiImage, CheckIfTestImagesCanBeFound)
@@ -329,10 +331,10 @@ TEST(SipiImage, Watermark)
   /* ASSERT_NO_THROW(img3.write("jpg", maoriWater)); */
 
   ASSERT_NO_THROW(img4.read(maoriWater));
-  EXPECT_TRUE(img4.compare(img3).value_or(1000) < 0.007); // 0.00605
+  EXPECT_TRUE(img4.compare(img3).value_or(1000) < 0.007);// 0.00605
 
   ASSERT_NO_THROW(img3.read(maori));
-  EXPECT_TRUE(img4.compare(img3) > 0.017); // 0.0174
+  EXPECT_TRUE(img4.compare(img3) > 0.017);// 0.0174
 
   ASSERT_NO_THROW(img3.rotate(90));
 }
@@ -377,4 +379,13 @@ TEST(SipiImage, TiffJpegAutoRgbConvert)
 
   EXPECT_NO_THROW(img.read(tiffJpegScanlineBug));
   EXPECT_NO_THROW(img.write("jpx", "../../../../test/_test_data/images/thumbs/tiffJpegScanlineBug.jp2"));
+}
+
+// Expect a crash when libjpeg reports a JWRN_HIT_MARKER.
+TEST(SipiImage, JpegCorruptWarn)
+{
+  Sipi::SipiIOTiff::initLibrary();
+  Sipi::SipiImage img;
+
+  EXPECT_THROW(img.read(jpegCorrupt), Sipi::SipiImageError);
 }
