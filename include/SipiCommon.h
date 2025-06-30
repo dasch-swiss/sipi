890ad2d870a12eb68b0554b68c3db6ad1c7cb1f3
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2025-06-30 10:42:59 +0200

    fixes for the CI/docker build

--- include/SipiCommon.h
+++ include/SipiCommon.h
@@ -9,18 +9,6 @@
 
 #include <cstdlib>
 
-namespace Sipi {
-
-/*!
- * We had to create a memcpy which will not dump a core if the data
- * is not aligned (problem with ubuntu!)
- *
- * @param to Adress where to copy the data to
- * @param from Adress from where the data is copied
- * @param len Number of bytes to copy
- */
-extern void memcpy(void *to, const void *from, size_t len);
-
-}// namespace Sipi
+namespace Sipi {}// namespace Sipi
 
 #endif// SIPI_SIPICOMMON_H
